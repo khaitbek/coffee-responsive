de0b8a75fc163a7a68f7e7144bc8d51560a6e8cb
khaitbek <khaitbek2005@gmail.com>
2022-08-12 06:52:07 +0500

    Finish header and hero

---
+++ script/nav.js
@@ -0,0 +1,9 @@
+const navToggle = document.getElementById("navToggle"),
+      primaryNav = document.getElementById("primaryNavigation"),
+      navList = document.querySelector(".navigation__list");
+
+navToggle.addEventListener("click",()=>{
+    navToggle.ariaExpanded = !navToggle.hasAttribute("toggle")
+    navToggle.toggleAttribute("toggle")
+    primaryNav.classList.toggle("opened")
+})
