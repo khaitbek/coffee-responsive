a114ad6d8b0061593934fb26ca6b5c8d83fdbfe8
khaitbek <khaitbek2005@gmail.com>
2022-08-18 05:57:05 +0500

    Finish plan page

--- script/nav.js
+++ script/nav.js
@@ -9,3 +9,26 @@ navToggle.addEventListener("click",()=>{
     console.log(document.body.attributes);
     primaryNav.classList.toggle("open")
 })
+
+const elModalOpenBtn = document.querySelector("#plan__btn")
+const elModalCloseBtn  = document.querySelector("#modal__checkout__btn")
+const modal = document.querySelector("#modal");
+
+elModalOpenBtn.addEventListener("click",(evt)=>{
+    document.body.style.overflowY = "hidden";
+    evt.preventDefault();
+    modal.classList.remove("close")
+    modal.showModal()
+})
+
+elModalCloseBtn.addEventListener("click",()=>{
+    closeModal();
+})
+
+function closeModal(){
+    document.body.style.overflowY = "visible";
+    modal.classList.add("close");
+    setTimeout(()=>{
+        modal.close();
+    },600)
+}
